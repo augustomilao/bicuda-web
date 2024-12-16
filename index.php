cadfcc56dfa297815d737f2d7c635310e37de622
Augusto Milão <augusto.milao01@gmail.com>
2024-12-16 20:04:24 -0300

    landing

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<body>
+    <?php
+
+    include 'views/landing.php';
+
+    ?>
+</body>
+
+</html>
---
+++ views/landing.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="util/style_landing.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Emilys+Candy&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <section class="section" id="section1">
+        <div style="display: grid; grid-template-columns: 50% 50%;">
+            <div style="margin: 10%; text-align: center;">
+                <h1 class="emilys-candy-regular">Transforme seu dia com o doce sabor da felicidade <br> venha se deliciar com nossas criações únicas!</h1>
+            </div>
+
+            <div>
+                <img style="width: 80%;" src="util/bicudinha.webp" alt="">
+            </div>
+        </div>
+    </section>
+
+    <h1 style="text-align: center;background-color: #2ecc71;">Conheça nossos produtos!</h1>
+    <section class="" id="section2">
+        <div style="display: grid; grid-template-columns: auto auto auto auto;";>  <!-- carrossel -->
+            <div style="text-align: center;"><img style="width: 80%;" src="util/bicudinha.webp" alt=""></div>
+            <div style="text-align: center;"><img style="width: 80%;" src="util/bicudinha.webp" alt=""></div>
+            <div style="text-align: center;"><img style="width: 80%;" src="util/bicudinha.webp" alt=""></div>
+            <div style="text-align: center;"><img style="width: 80%;" src="util/bicudinha.webp" alt=""></div>
+        </div>
+    </section>
+    <section class="section" id="section3">
+        <h1>Seção 3</h1>
+    </section>
+</body>
+
+</html>
